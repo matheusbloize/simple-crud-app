2352aeaddaf72c09a8da6ec90a159222df10b9fc
Matheus Bloize <mbloizepd@gmail.com>
2023-07-15 11:14:21 -0300

    feat: database connected

--- server/server.js
+++ server/server.js
@@ -1,10 +1,18 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+const MONGODB_URL = process.env.MONGODB_URL;
+const PORT = process.env.PORT
+
+mongoose.connect(MONGODB_URL).then(() => {
+  console.log('connected to mongodb');
+  app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+  });
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
-});
-
-app.listen(8000, () => {
-  console.log('server running on port 8000');
 });
